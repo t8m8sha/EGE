d06b27f994fe0aac5c05ec663f66062208f0d1c0
t8m8sha <timurpanfilov406@gmail.com>
2025-04-18 19:46:43 +0200

    volya

--- 18.04.2025/Task-13-10169.py
+++ 18.04.2025/Task-13-10169.py
@@ -8,4 +8,6 @@ for mask in range(33):
     net1 = ip_network(f'{ip1}/{mask}',False)
     net2 = ip_network(f'{ip2}/{mask}', False)
     if net1.network_address == net2.network_address:
-        print(mask)
+        if ip1 not in (net1.network_address,net1.broadcast_address):
+            if ip2 not in (net1.network_address, net2.broadcast_address):
+                print(net1.num_addresses)
