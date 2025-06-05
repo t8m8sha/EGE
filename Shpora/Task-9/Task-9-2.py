1aea2291a72c7adf95e612aa95f9d005198edd9e
t8m8sha <timurpanfilov406@gmail.com>
2025-06-05 11:58:54 +0200

    salem

--- Shpora/Task-9/Task-9-2.py
+++ Shpora/Task-9/Task-9-2.py
@@ -6,4 +6,8 @@ def f(nums):
     return  cnt.count(2) == 4 and cnt.count(1) == 3
 
 def f1(nums):
-    cnt =
+    return nums.count(max(nums)) == 1
+for i in nums:
+    if f(i) and f1(i):
+        print(sum(i))
+        break
